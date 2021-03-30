9652875ddae9522ec65b73caff8abfb5d64ebecd
SeriousIgor <seriousigorok@gmail.com>
2021-03-30 14:17:38 +0300

    Fixed mistakes

--- Homework4/src/Car.java
+++ Homework4/src/Car.java
@@ -12,22 +12,22 @@ public class Car {
         fuelConsumption = consumption;
     }
     //methods
-    void GetFuelBalance(){                        //Вывод кол-во топлива в баке
+    void getFuelBalance(){                        //Вывод кол-во топлива в баке
         System.out.println("В баке " + fuelBalance + " литров");
     }
     public double Refuel(double price){             //заполняет бак, возвращает цену заправки
         fuelBalance += fuelToRefuel;
         sum = fuelToRefuel * price;
         System.out.println("Заправляем бак\nВж-ж-ж-ж");
-        GetFuelBalance();
+        getFuelBalance();
         return sum;
     }
-    public void GetFuelBalanceAfterRide(int km){    //Выводит кол-во топлива в баке после N километров
+    public void getFuelBalanceAfterRide(int km){    //Выводит кол-во топлива в баке после N километров
         fuelBalance -= (fuelConsumption * km)/100;
-        GetFuelBalance();
-        HowMuchFuel();
+        getFuelBalance();
+        howMuchFuel();
     }
-    public void HowMuchFuel(){                      //Определяет, сколько топлива нужно до заправить
+    public void howMuchFuel(){                      //Определяет, сколько топлива нужно до заправить
         fuelToRefuel = fuelVolume - fuelBalance;
         System.out.print("Нужно дозаправить ");
         System.out.format("%.2f",fuelToRefuel);
--- Homework4/src/CarDemo.java
+++ Homework4/src/CarDemo.java
@@ -1,33 +1,38 @@
 public class CarDemo {
     public static void main(String[] args) {
-        double fuelPrice = Integer.parseInt(args[0]);                 //Цена топлива
-        double sumOfTrip = 0;                                         //Определяет сумму всей поездки
-        int krOz = 178;                                               //Расстояние от Одессы до Кривого Озера
-        int zhashkow = 153;                                           //Расстояние от Кривого Озера до Жашкова
-        int kiyv = 148;                                               //Расстояние от Жашкова до Киева
+        double fuelPrice = 0;                 //Цена топлива
+        double sumOfTrip = 0;                                               //Определяет сумму всей поездки
+        final int startPoint = 0;                                           //Начальная позиция
+        final int krOz = 178;                                               //Расстояние от Одессы до Кривого Озера
+        final int zhashkow = 153;                                           //Расстояние от Кривого Озера до Жашкова
+        final int kiyv = 148;                                               //Расстояние от Жашкова до Киева
 
         Car zaporozhec = new Car(60, 30, 8); //создаёт экземпляр класса Car под названием zaporozhec
 
+        if (args.length != 0) {
+            fuelPrice = Integer.parseInt(args[0]);
+        }
+
         System.out.println("Начало поездки. Заправка в Одессе");
-        zaporozhec.GetFuelBalanceAfterRide(0);
+        zaporozhec.getFuelBalanceAfterRide(startPoint);
         sumOfTrip += zaporozhec.Refuel(fuelPrice);
 
         System.out.println();
 
         System.out.println("Первая остановка - Кривое Озеро.");
-        zaporozhec.GetFuelBalanceAfterRide(krOz);
+        zaporozhec.getFuelBalanceAfterRide(krOz);
         sumOfTrip += zaporozhec.Refuel(fuelPrice);
 
         System.out.println();
 
         System.out.println("Вторая остановка - Жашков.");
-        zaporozhec.GetFuelBalanceAfterRide(zhashkow);
+        zaporozhec.getFuelBalanceAfterRide(zhashkow);
         sumOfTrip += zaporozhec.Refuel(fuelPrice);
 
         System.out.println();
 
         System.out.println("Пункт прибытия - Киев.");
-        zaporozhec.GetFuelBalanceAfterRide(kiyv);
+        zaporozhec.getFuelBalanceAfterRide(kiyv);
 
         System.out.println("Расходы на топливо - " + sumOfTrip + " у.е.");
     }
