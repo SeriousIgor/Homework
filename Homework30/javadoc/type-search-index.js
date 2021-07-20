53c585e586536d0a6283df8122d50bd8843c967f
SeriousIgor <seriousigorok@gmail.com>
2021-07-20 03:02:49 +0300

    Added Homework30: Writing custom cache (using JUnit, JavaDoc, Logs).

---
+++ Homework30/javadoc/type-search-index.js
@@ -0,0 +1,1 @@
+typeSearchIndex = [{"l":"All Classes","url":"allclasses-index.html"},{"p":"<Unnamed>","l":"CustomCache"},{"p":"<Unnamed>","l":"CustomCacheRun"},{"p":"<Unnamed>","l":"CustomCacheTest"},{"p":"<Unnamed>","l":"ICache"}]
